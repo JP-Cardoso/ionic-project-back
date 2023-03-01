6b5c895f7d501daf901572a569b3bae7ee877669
JP-Cardoso <jhonpoal@hotmail.com>
2023-02-28 23:14:43 -0300

    add das variaveis de ambiente

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,17 @@
+require('dotenv').config()
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
+const DB = process.env.DB_USER;
+const Password = process.env.DB_DB_PASSWORD
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "1234",
-    database: "getmestres",
+    password: Password,
+    database: DB,
     synchronize: true,
     logging: false,
     entities: [User],
